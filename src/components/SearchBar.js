55532f127692395a948f12f9955addc0cc696867
healthy-vitamins <cheeaudrian97@gmail.com>
2023-01-02 19:21:26 +0800

    test build 2

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
 import { Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { addWords, resetWords } from "../features/suggestedWordsSlice";
