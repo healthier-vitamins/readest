9dfff55ec8a3689c3bd5212d95ce200410fd9247
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2023-01-23 23:40:46 +0800

    testing post word

--- netlify/functions/fetchNotion.js
+++ netlify/functions/fetchNotion.js
@@ -1,4 +1,5 @@
 const { Client } = require("@notionhq/client");
+const { wordSchema } = require("../../src/utils/wordTemplate");
 
 const { NOTION_KEY, NOTION_DB_BOOK_KEY } = process.env;
 const notion = new Client({
@@ -6,11 +7,13 @@ const notion = new Client({
 });
 
 exports.handler = async function (event, context) {
+  console.log(wordSchema.WORD);
   try {
     const response = await notion.pages.retrieve({
-      page_id: "490181b3-45f5-477f-a631-8a73cd41cb71",
+      page_id: "bcf66db723304d96b02cc0870a8aabe5",
+      // parent: "490181b3-45f5-477f-a631-8a73cd41cb71"
     });
-    
+
     // console.log(event);
     // console.log(context);
     // console.log(response.results[0].properties.Words);
--- netlify/functions/postWord.js
+++ netlify/functions/postWord.js
@@ -1,42 +1,73 @@
 const { Client } = require("@notionhq/client");
 const { HttpStatusCode } = require("axios");
+const { wordSchema } = require("../../src/utils/wordTemplate");
 
-const { NOTION_KEY } = process.env;
+const { NOTION_KEY, NOTION_DB_WORD_KEY } = process.env;
 const notion = new Client({
   auth: NOTION_KEY,
 });
 
 exports.handler = async function (event, context) {
-  const { selectedBooks, wordDefinition } = JSON.parse(event.body);
-  selectedBooks.forEach(async (book) => {
-    console.log("individual bookies: ", book);
-    console.log("word def in api: ", wordDefinition);
-    try {
-      const response = await notion.pages.create({
-        parent: {
-          page_id: book.id,
-        },
-        properties: {
-          Word: {
-            title: [
-              {
-                text: {
-                  content: wordDefinition.title,
-                },
-              },
-            ],
+  //   const { selectedBooks, wordDefinition } = JSON.parse(event.body);
+  //   selectedBooks.forEach(async (book) => {
+  //     console.log("individual bookies: ", book);
+  //     console.log("word def in api: ", wordDefinition);
+
+  try {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: NOTION_DB_WORD_KEY,
+        type: "database_id",
+      },
+      properties: {
+        [wordSchema.TITLE]: [
+          {
+            text: {
+              content: "woooooo",
+            },
+          },
+        ],
+        [wordSchema.WORD]: [
+          {
+            text: {
+              content: "woooooo",
+            },
+          },
+        ],
+        [wordSchema.STATUS]: {
+          select: {
+            name: "Live",
           },
         },
-      });
-      return {
-        statusCode: HttpStatusCode.Ok,
-        body: JSON.stringify(response),
-      };
-    } catch (err) {
-      return {
-        statusCode: HttpStatusCode.InternalServerError,
-        body: err.toString(),
-      };
-    }
-  });
+        [wordSchema.DEFINITION]: {
+          rich_text: [
+            {
+              text: {
+                content: "wooooo definition",
+              },
+            },
+          ],
+        },
+        [wordSchema.ABBREVIATION]: {
+          rich_text: [
+            {
+              text: {
+                content: "wooooo abbreviation",
+              },
+            },
+          ],
+        },
+      },
+    });
+    return {
+      statusCode: HttpStatusCode.Ok,
+      body: JSON.stringify(response),
+    };
+  } catch (err) {
+    return {
+      statusCode: HttpStatusCode.InternalServerError,
+      body: err.toString(),
+    };
+  }
+  //   });
 };
