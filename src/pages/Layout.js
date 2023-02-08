2392ce60dcff36f33d15a73e0219d98dd284ac7b
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2023-02-09 03:14:50 +0800

    support: fixed overflow issue on both computer and mobile screen; feat: added sticky behaviour for navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,12 +2,29 @@ import { Outlet } from "react-router";
 import CreateBookModal from "../components/modal/CreateBookModal";
 import SaveWordModal from "../components/modal/SaveWordModal";
 import NavBar from "../components/navBar/NavBar";
+import useWindowDimensions from "../utils/useWindowDimensions";
+import "./Layout.css";
 
 function Layout() {
+  const { height } = useWindowDimensions();
+
+  function heightLogic() {
+    return {
+      height: height,
+    };
+  }
+
   return (
     <>
-      <NavBar></NavBar>
-      <Outlet></Outlet>
+      {console.log("height ||||||||||||||||||||||| ", height)}
+      <div className="mainest" style={heightLogic()}>
+        <div className="all-navbar">
+          <NavBar></NavBar>
+        </div>
+        <div className="outlet">
+          <Outlet></Outlet>
+        </div>
+      </div>
       <CreateBookModal></CreateBookModal>
       <SaveWordModal></SaveWordModal>
     </>
