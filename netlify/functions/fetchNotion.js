89bc8684fc7c88bba42e1869b0d9497090d6979b
Audrian Heng <cheeaudrian97@gmail.com>
2023-03-24 10:09:56 +0800

    support: found user child database

--- netlify/functions/fetchNotion.js
+++ netlify/functions/fetchNotion.js
@@ -8,9 +8,11 @@ const notion = new Client({
 exports.handler = async function (event, context) {
   try {
     const response = await notion.blocks.children.list({
-      // const response = await notion.databases.query({
-      // page_id: "25f5492dc2e74f618c923c1e58a3eb55",
-      block_id: "25f5492dc2e74f618c923c1e58a3eb55",
+      //? user account page id
+      // block_id: "552976822f714655a049d60dc8462368",
+
+      //? user account child database id
+      block_id: "f5fd916a-a5d5-4867-956d-b2bd8c53c3c7",
 
       // page_id: "2ca52834-8db0-4595-bc9b-ed565deb1748",
       // database_id: "473d8c52-4d47-4d13-b5d5-d2aee88ae5d0",
