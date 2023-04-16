8bd7080fbab664bd5701c357c8e6622202bb68d7
healthy-vitamins <cheeaudrian97@gmail.com>
2023-04-17 03:40:01 +0800

    feature: initialised YearTimer

--- src/components/yearTimer/YearTimer.tsx
+++ src/components/yearTimer/YearTimer.tsx
@@ -1,17 +1,74 @@
 import moment from "moment-timezone";
-import { useEffect, useState } from "react";
+import React, { JSXElementConstructor, useEffect, useState } from "react";
+import "./YearTimer.scss";
 
 function YearTimer() {
-  // const [date, setDate] = useState()
+  const [yearRemainingPercentage, setYearRemainingPercentage] = useState<
+    number | null
+  >(null);
+  // const [yearProgressPercentage, setYearProgressPercentage] = useState<
+  //   string | null
+  // >(null);
 
   useEffect(() => {
     moment.tz.setDefault("Asia/Singapore");
     const today = moment();
     const endOfYear = moment().endOf("year");
     const startOfYear = moment().startOf("year");
+
+    const daysRemaining = endOfYear.diff(today, "days");
+    const totalDays = endOfYear.diff(startOfYear, "days");
+    // console.log(daysRemaining);
+    // console.log(totalDays);
+    const remainingPercentage = (daysRemaining / totalDays) * 100;
+    // const progressPercentage = startOfYear.diff(today)
+    console.log(Math.round(remainingPercentage / 10));
+
+    setYearRemainingPercentage(Math.round(remainingPercentage / 10));
   }, []);
 
-  return <>{console.log(moment().toDate())}</>;
+  function renderRemaining(): React.ReactElement[] | React.ReactElement {
+    if (yearRemainingPercentage) {
+      // for (let i = 0; i < yearRemainingPercentage; i++) {
+      //   console.log(yearRemainingPercentage);
+      //   <span className="timer-progress-shade">&#x2591;</span>;
+      // }
+      const array = Array(yearRemainingPercentage).fill(1);
+      console.log(array);
+      return array.map((ele) => (
+        <span className="timer-progress-shade">&#x2591;</span>
+      ));
+    }
+    return <></>;
+  }
+  function renderProgress(): React.ReactElement[] | React.ReactElement {
+    if (yearRemainingPercentage) {
+      // for (let i = 0; i < yearRemainingPercentage; i++) {
+      //   console.log(yearRemainingPercentage);
+      //   <span className="timer-progress-shade">&#x2591;</span>;
+      // }
+      const progress = 10 - yearRemainingPercentage;
+      const array = Array(progress).fill(1);
+      console.log(array);
+      return array.map((ele) => (
+        <span className="timer-progress-shade">&#x2593;</span>
+      ));
+    }
+    return <></>;
+  }
+
+  return (
+    <React.Fragment>
+      {yearRemainingPercentage && (
+        <div className="timer-container">
+          <div className="timer-progress-bar">
+            {renderProgress()}
+            {renderRemaining()}
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default YearTimer;
